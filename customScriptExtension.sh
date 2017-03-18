0f138051f8954af7f826cc0567bcd964ceb52d59
Rakesh Varna <ravarna@microsoft.com>
2017-03-18 11:16:09 -0700

    Added comments to script.

--- customScriptExtension.sh
+++ customScriptExtension.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+# Many a times, we see VMs in VMSS not being set up fully yet (pending investigation).
 sleep 30
-echo "Starting docker installation at `date`" >> test.txt
+echo "Starting docker installation at `date`" >> log.txt
 curl -sSL https://get.docker.com/ | sh
-echo "Completed docker installation at `date`" >> test.txt
-echo "Starting docker container run at `date`" >> test.txt
+echo "Completed docker installation at `date`" >> log.txt
+
+echo "Starting docker container run at `date`" >> log.txt
 docker run -e EULA=TRUE -d -p 25565:25565 --name mc itzg/minecraft-server
-echo "Completed docker container run at `date`" >> test.txt
+echo "Completed docker container run at `date`" >> log.txt
