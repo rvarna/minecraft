ef09f9552b2f0269ae70af1bb86338147430ceed
Rakesh Varna <ravarna@microsoft.com>
2017-03-30 15:52:08 -0700

    Added check for cmdline parameters.

--- aiScript.sh
+++ aiScript.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
-
-format='{
-    "name":  "Microsoft.ApplicationInsights.Event",
-    "time":  "#datetime",
-    "iKey":  "#aiInstrumentationKey",
-    "tags":  {
-                 "ai.cloud.roleInstance":  "#hostname",
-                 "ai.internal.sdkVersion":  "dotnet:2.3.0-32439"
-             },
-    "data":  {
-                 "baseType":  "EventData",
-                 "baseData":  {
-                                  "ver":  2,
-                                  "name":  "myStartLinuxEvent",
-                                  "properties":  {
-                                                     "correlationid":  "#correlationid"
-                                                 }
-                              }
-             }
-}';
-datetime=`date -Iseconds`;
-hostnameValue=`hostname`;
-format=${format/\#datetime/$datetime};
-format=${format/\#hostname/$hostnameValue};
-format=${format/\#aiInstrumentationKey/$1};
-format=${format/\#correlationid/$2};
-curl -H "Content-Type: application/json" -X POST -d "$format" https://dc.services.visualstudio.com/v2/track
+if [ ! -z "$2"]; then
+    format='{
+        "name":  "Microsoft.ApplicationInsights.Event",
+        "time":  "#datetime",
+        "iKey":  "#aiInstrumentationKey",
+        "tags":  {
+                    "ai.cloud.roleInstance":  "#hostname",
+                    "ai.internal.sdkVersion":  "dotnet:2.3.0-32439"
+                },
+        "data":  {
+                    "baseType":  "EventData",
+                    "baseData":  {
+                                    "ver":  2,
+                                    "name":  "myStartLinuxEvent",
+                                    "properties":  {
+                                                        "correlationid":  "#correlationid"
+                                                    }
+                                }
+                }
+    }';
+    datetime=`date -Iseconds`;
+    hostnameValue=`hostname`;
+    format=${format/\#datetime/$datetime};
+    format=${format/\#hostname/$hostnameValue};
+    format=${format/\#correlationid/$1};
+    format=${format/\#aiInstrumentationKey/$2};    
+    curl -H "Content-Type: application/json" -X POST -d "$format" https://dc.services.visualstudio.com/v2/track
+fi
